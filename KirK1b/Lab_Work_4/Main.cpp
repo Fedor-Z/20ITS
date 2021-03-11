22d5e650b31821e36d51923f515e634f5cced19d
KirK1b <newstepof505@gmail.com>
2021-03-11 18:41:30 +0300

    Code update

--- KirK1b/Lab_Work_4/Main.cpp
+++ KirK1b/Lab_Work_4/Main.cpp
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 #include <windows.h>
+//Var - 1
 
-/*Харламов К.Д. 
-Вариант 1*/
-
-int func(int trsl[], int count);
-int func2(int trsl[], int count);
+int DecodeSmallValue(int trsl[], int count);
+int DecodingBigValue(int trsl[], int count);
+int bitrate(int binar);
 
 int main() {
 	char ascii[10] = { '0','1','2','3','4','5','6','7','8','9' };
 	unsigned char input[3];
 	int trsl[10] = { 0,1,2,3,4,5,6,7,8,9 };
-	int count = 0, check = 0, binary = 0;
-	int layer = 0, type = 0, bitrate = 0, cs = 0;
+	int count = 0, iter = 0, binary = 0;
+	int layer = 0, cs = 0;
 	printf("Write value in range of [0;255]:\n");
 	scanf_s("%3c", input);
 	if (input[2] == '\n' && input[1]!='\n') {
@@ -26,58 +25,45 @@ int main() {
 		input[1] = '0';
 		input[0] = '0';
 	}
-	while (check != 1) {
+	for (count; count < 10; count++) {
 		if (input[0] == ascii[count]) {
 			binary = trsl[count] * 100;
-			check = 1;
 			break;
 		}
-		count++;
 	}
 	count = 0;
-	check = 0;
-	while (check != 1) {
+	for(count; count<10;count++) {
 		if (input[1] == ascii[count]) {
 			binary = binary + trsl[count] * 10;
-			check = 1;
 			break;
 		}
-		count++;
 	}
-	check = 0;
 	count = 0;
-	while (check != 1) {
+	for(count; count < 10; count++) {
 		if (input[2] == ascii[count]) {
 			binary = binary + trsl[count];
-			check = 1;
 			break;
 		}
-		count++;
 	}
-	check = 0;
 	count = 0;
 
-	while (check != 8) {
+	for (iter; iter < 8; iter++) { 
 		if (count == 0) {
-			if (trunc(binary / (128 / pow(2, check))) == 1) {
-				count = binary % (int)(128 / pow(2, check));
-				trsl[check] = 1;
-				check++;
+			if (trunc(binary / (128 / pow(2, iter))) == 1) {
+				count = binary % (int)(128 / pow(2, iter)); 
+				trsl[iter] = 1;
 			}
 			else {
-				trsl[check] = 0;
-				check++;
+				trsl[iter] = 0;
 			}
 		}
 		else {
-			if (trunc(count / (128 / pow(2, check))) == 1) {
-				count = count % (int)(128 / pow(2, check)); 
-				trsl[check] = 1;
-				check++;
+			if (trunc(count / (128 / pow(2, iter))) == 1) {
+				count = count % (int)(128 / pow(2, iter)); 
+				trsl[iter] = 1;
 			}
 			else {
-				trsl[check] = 0;
-				check++;
+				trsl[iter] = 0;
 			}
 		}
 
@@ -119,36 +105,12 @@ int main() {
 		}
 		}
 	}
-	//type
-	if (trsl[2] == 0) {
-	printf("Type - CBR\n");
-	printf("Bitrate info:\n");
-	if (trsl[3] == 0) printf("Mode - mono\n");
-	else printf("Mode - stereo\n");
-	if (func2(trsl, 4) == 0) printf("Bitrate - 32 kbps");
-	if (func2(trsl, 4) == 1) printf("Bitrate - 48 kbps");
-	if (func2(trsl, 4) == 2) printf("Bitrate - 64 kbps");
-	if (func2(trsl, 4) == 3) printf("Bitrate - 96 kbps");
-	if (func2(trsl, 4) == 4) printf("Bitrate - 128 kbps");
-	if (func2(trsl, 4) == 5) printf("Bitrate - 192 kbps");
-	if (func2(trsl, 4) == 6) printf("Bitrate - 256 kbps");
-	if (func2(trsl, 4) == 7) printf("Bitrate - 320 kbps");
-}
-	else {
-		printf("Type - VBR\n");
-		if (func(trsl, 3) == 0) printf("Range: 32-48 kbps\n");
-		if (func(trsl, 3) == 1) printf("Range: 64-96 kbps\n");
-		if (func(trsl, 3) == 2) printf("Range: 128-192 kbps\n");
-		if (func(trsl, 3) == 3) printf("Range: 256-320  kbps\n");
-		if (func(trsl, 5) == 0) printf("Mode: stereo\n");
-		if (func(trsl, 5) == 1) printf("Mode: joint stereo\n");
-		if (func(trsl, 5) == 2) printf("Mode: dual channel\n");
-		if (func(trsl, 5) == 3) printf("Mode: sigle channel\n");
-		}
+	
+	bitrate(binary);
 
 	}
 
-	int func(int trsl[], int count) {
+	int DecodeSmallValue(int trsl[], int count) {
 		switch (trsl[count]) {
 		case 0: {
 			if (trsl[count + 1] == 0) return 0; 
@@ -161,7 +123,7 @@ int main() {
 		}
 	}
 
-	int func2(int trsl[], int count) {
+	int DecodingBigValue(int trsl[], int count) {
 		switch (trsl[count]) {
 		case 0: {
 			if (trsl[count+1] == 0) {
@@ -182,9 +144,50 @@ int main() {
 				if (trsl[count + 2] == 0) return 6;
 				else return 7;
 			}
-		}
+			}
 		}
 	}
-	
-
-
+	 
+	int bitrate(int binar){
+			int transl = 0; // 7 - 111
+			//Bitrate mode
+			int mode = binar;
+			transl = binar << 3;
+			transl = binar >> 7;
+			if (transl == 1) {
+				printf("Type - VBR\n");
+				binar = binar << 3;
+				binar = binar >> 4;
+				//range
+				binar = binar >> 2;
+				transl = binar & 0x3;
+					if(transl==0) printf("Range: 32-48 kbps\n");
+					if(transl==1) printf("Range: 64-96 kbps\n");
+					if(transl==2) printf("Range: 128-192 kbps\n");
+					if(transl==3) printf("Range: 256-320  kbps\n");
+				//mode
+					mode = mode << 5;
+					mode = mode >> 6;
+					transl = mode & 0x3;
+					if (transl == 0) printf("Mode: stereo\n");
+					if (transl == 1) printf("Mode: joint stereo\n");
+					if (transl == 2) printf("Mode: dual channel\n");
+					if (transl == 3) printf("Mode: sigle channel\n");
+			}
+			else {
+				printf("Type - CBR\n");
+				binar = binar << 4;
+				binar = binar >> 5;
+				transl = binar & 0x7;
+				for (int e = 0; e < 8; e++) {
+					if (transl == e) {
+						if (e < 3) transl = 32 + e * 16;
+						else if (e >= 3 && e < 5) transl = e * 32;
+						else transl = 64 * e - 32 * (e - (trunc(e / 2) - 1)); //дотестить на числах 6 и 7
+						printf("Bitrate - %d kbps", transl);
+						break;
+					}
+				}
+			}
+			return 0;
+		}
