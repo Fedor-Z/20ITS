7ab9ef9ffd2640a7cfbab275f91f4f44f45add98
Makh <@>
2021-03-30 12:07:44 +0300

    1 вариант Только подсчет символов в файле txt

---
+++ Makh/Laba 5/Laba 5/Laba 5/Laba 5.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{
+    FILE* fp=NULL;
+    int num=0;
+    fp = fopen("C:/Users/alexm/Desktop/Visual Studio/20ITS/Makh/Laba 5/input.txt", "r+");
+    while (fgetc(fp)!=EOF)
+    {
+        num++;
+    }
+    printf("%d", num);
+    //fclose(fp);
+}
