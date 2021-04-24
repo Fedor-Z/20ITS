7e946ead33fb455d02a20a268a13891d4c8f66d0
DmitrySuvorov <dimaamid477@gmail.com>
2021-04-25 02:25:16 +0300

    Полностью готовая Лаба 5

--- Suvorov/Dmitry Suvorov Lab 5/Dmitry Suvorov Lab 5/Dmitry Suvorov Lab 5.cpp
+++ Suvorov/Dmitry Suvorov Lab 5/Dmitry Suvorov Lab 5/Dmitry Suvorov Lab 5.cpp
@@ -1,88 +1,100 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
 
 #define SENTENCE_ENDS ".?!"
 using namespace std;
 
-void probel(char* dest, const char* source)
+void probel(char* dest, const char* source, int i)
 {
-    for (; (*dest = *source); ++source, ++dest)
-    {
-        if (*source != ' ')
-            continue;
-        *(dest++) = *source;
-        for (; *source == ' '; ++source)
-            ;
-        *dest = *source;
-    }
+	for (; (*dest = *source); ++source, ++dest)
+	{
+		if (*source != ' ')continue;
+		*(dest++) = *source;
+
+		for (; *source == ' '; ++source);
+
+		*dest = *source;
+		if (*source == (char)34 || *source == (char)39) {
+			memcpy(dest, source, i);
+			source = source + i; //пропускаем элементы в кавычках
+			dest = dest + i - 1; //убираем лишний элемент с конца
+		}
+	}
 }
+
+int sprint(char* str, char* kav, int k)
+{
+	int pos = strcspn(str, kav); //ищем 1 скобку
+	int i = pos;
+	str[pos] = 32; //записываем пробел
+	pos = strcspn(str, kav); //ищем вторую скобку
+	str[i] = *kav;
+	k = pos + 1;
+	return k;
+}
+
 
 int main()
 {
-    setlocale(LC_ALL, "Rus");
-    FILE* text;
-    const char* filename = "C:/Users/dimaa/Desktop/DmitrySuvorov/20ITS/Suvorov/Dmitry Suvorov Lab 5/text.txt";
-    char result_string[100];
-    text = fopen(filename, "r");
+	setlocale(LC_ALL, "Rus");
+	FILE* text;
+	const char* filename = "C:/Users/dimaa/Desktop/DmitrySuvorov/20ITS/Suvorov/Dmitry Suvorov Lab 5/text.txt";
+	char result_string[100];
+	text = fopen(filename, "r");
 
-    if (text == NULL)
-    {
-        printf("Не могу открыть файл '%s'", filename);
-        return 0;
-    }
+	char* kavv = (char*)malloc(1 * sizeof(char));
+	char* apostr = (char*)malloc(1 * sizeof(char));
+	*apostr = 39;
+	*kavv = 34;
+	int point = 0;
+	int lenght = 0;
 
+	if (text == NULL)
+	{
+		printf("Не могу открыть файл '%s'", filename);
+		return 0;
+	}
 
-    printf("Редактированный текст из файла: \n\n");
+	printf("Редактированный текст из файла: \n\n");
 
-    while (fgets(result_string, sizeof(result_string), text))
-    {
-        probel(result_string, result_string);
+	while (fgets(result_string, sizeof(result_string), text))
+	{
 
-        char* p;
-        int inSentence;
+		if (strlen(result_string) != 0)
+		{
+			int r = 0;
+			while (result_string[r] != '\0')
+			{
 
-        for (inSentence = 0, p = result_string; *p != '\0'; ++p)
-        {
-            if (strchr(SENTENCE_ENDS, *p))
-                inSentence = 0;
-            else if (isspace(*p)) {}
-            else {
-                if (!inSentence)
-                    *p = toupper(*p);
-                inSentence = 1;
-            }
-        }//Для выражения после точки
+				if (result_string[r] == *kavv) {
+					lenght = r;
+					r = sprint(result_string, kavv, r);
+					lenght = r - lenght;
+				}
+				if (result_string[r] == *apostr) {
+					lenght = r;
+					r = sprint(result_string, apostr, r);
+					lenght = r - lenght;
+				}
 
+				if (result_string[r] == '.' || point != 0)
+				{
+					while (result_string[r + 1] == ' ') r++; //пропускаем пробелы
 
-        if (result_string[strlen(result_string) - 1] == '\n')
-        {
-            result_string[strlen(result_string) - 1] = '\0';
-            int r = 0;
-            while (result_string[r] != '\0')
-            {
-                if (result_string[r] == '.')
-                {
-                    if (result_string[r + 1] == ' ')
-                    {
-                        if ((result_string[r + 2] >= 97) && (result_string[r + 2] <= 122))
-                        {
-                            result_string[r + 2] = toupper(result_string[r + 2]);
-                            r++;
-                        }
-                        else r++;
-                    }
+					if ((result_string[r + 1] >= 97) && (result_string[r + 1] <= 122) && (result_string[r + 1] != *kavv) && (result_string[r + 1] != *apostr))
+					{
+						result_string[r + 1] = toupper(result_string[r + 1]);
+						point = 0;
+					}
 
-                    if ((result_string[r + 1] >= 97) && (result_string[r + 1] <= 122))
-                    {
-                        result_string[r + 1] = toupper(result_string[r + 1]);
-                        r++;
-                    }
-                    else r++;
-                }
-                else r++;
-            }
-        };//Для комбинации после точки и пробела
+				}
+				r++;
+			}
+		};
 
-        printf("%s\n", result_string);
-    }
-    fclose(text);
+		probel(result_string, result_string, lenght);
+		printf("%s", result_string);
+	}
+	fclose(text);
 }
